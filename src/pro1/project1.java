c1b9bc2f97a6c1c1575850e92bbb2b66fc67fcda
ahmadabuadas <adas.2016@yahoo.com>
2018-06-14 00:05:47 -0500

    first commit for personal app card by java

---
+++ src/pro1/project1.java
@@ -0,0 +1,25 @@
+package pro1;
+
+public class project1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("+-------------------------------------------------------+");
+System.out.println("| \t\t\t\t\t\t4564    |");
+System.out.println("| \t\t\t\t\t\t5432    |");
+System.out.println("| \t\t\t\t\t\t7656    |");
+
+System.out.println("| \t\t\t\t\t\t\t|");
+System.out.println("| \t\t\t\t\t\t\t|");
+System.out.println("| \t\t\t\t\t\t\t|");
+
+System.out.println("| \t\t\t\tAhmad abuadas\t\t|");
+System.out.println("| \t\t\t\t8543 ridge ave\t\t|");
+System.out.println("| \t\t\t\tChecago,IL 60600\t|");
+
+System.out.println("| \t\t\t\t\t\t\t|");
+
+System.out.println("+-------------------------------------------------------+");
+
+	}
+}
